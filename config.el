894b38366d020fc9873c3f4f7c04f3e10967e6f0
Aleksandr Mamonov <mamonov116km@gmail.com>
2026-01-16 15:20:14 +0100

    org, org-roam
    try to make it compatible with logseq

--- config.el
+++ config.el
@@ -44,6 +44,12 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+(setq org-roam-directory "/Users/aleksandr/Documents/Notes")
+(setq org-roam-dailies-directory "/Users/aleksandr/Documents/Notes/journals")
+
+(after! org-roam
+  (setq org-roam-file-exclude-regexp
+        (regexp-opt '("/logseq/" "/assets/"))))
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
